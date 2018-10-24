86c54f44f7dc7c21b35969afdb12b853462a515c
SeanyDcode <38363783+SeanyDcode@users.noreply.github.com>
2018-10-24 08:47:02 -0400

    Create dailychallenge1.py
    First attempt at Daily Coding Challenge 1.

---
+++ dailychallenge1.py
@@ -0,0 +1,29 @@
+# From dailycodingproblem.com
+#
+# Daily Challenge 1
+# Given a list of numbers and number k, return whether any two numbers from the list add up to k.
+#
+# For example, give [10, 15, 3, 7] and k of 17, return true since 10 + 7 = 17.
+
+array = [10, 15, 3, 7]
+k = 17
+
+length = len(array)
+
+# iterate through all array values except last one
+for i in range(length - 1):
+  
+  # initialize counter for comparing other values in array with current value
+  j = i + 1
+  
+  # find remainder to compare to other array values 
+  remainder = k % array[i]
+  
+  # iterate through other values after ith value in array
+  while j < length:
+  
+    # if remainder matches another value in array, return True
+    if remainder == array[j]:
+      return True
+      
+return False  

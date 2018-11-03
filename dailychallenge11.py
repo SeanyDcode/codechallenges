c31948a27b780fa95bcdbfaaaeade748c729e693
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2018-11-03 17:24:26 -0400

    Update challenge11.py

--- dailychallenge11.py
+++ dailychallenge11.py
@@ -1,6 +1,6 @@
 # from dailycodingproblem.com
 #
-# Daily Challenge #9
+# Daily Challenge #11
 # Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, 
 # return all strings in the set that have s as a prefix.
 #

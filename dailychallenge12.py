fb79af1b3f6273da2d114f98a71389798e5f0e6e
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2018-11-04 15:13:12 -0500

    Update dailychallenge12.py
    Working solution using dynamic programming

--- dailychallenge12.py
+++ dailychallenge12.py
@@ -14,45 +14,26 @@
 # What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of 
 # positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
 
-def recurseSteps(steps, strides, returnList):
-    
-    # if no steps remaining, return final list
-    if steps == 0:
-        return returnList
-    
-    # if steps remaining, continue to count step combinations
-    else:
-        
-        # for each potential step combination
-        for stride in strides:
-            
-            # CHECK FOR POTENTIAL DIVISION ABILITY WITH REMAINING STEPS
-            
-            # check to make sure stride is not larger than total steps remaining
-            if steps - stride >= 0:
-                # subtract stride from total steps remaining
-                steps -= stride
-                # add stride to returnList
-                returnList.append(stride)
-                # continue to recurse with function
-                recurseSteps(steps, strides, returnList)
-            else:
-                pass
+def staircase(n, X):
+    # create list for storing potential combinations for each step
+    cache = [0 for _ in range(n + 1)]
+    # initialize for step 1
+    cache[0] = 1
+    # add value for each step based on value for previous step
+    for i in range(n + 1):
+        cache[i] += sum(cache[i - x] for x in X if i - x > 0)
+        cache[i] += 1 if i in X else 0
+    # return last value from list which is the total number of possible combinations
+    return cache[-1]
 
 
 def main():
-    uniqueLists = []
-    x = [1, 2]
-    n = 4
+    X = [1, 3, 5]
+    n = 9
 
-    # iterate through possible last steps
-    for i in x:
-      
-      # call recursive function to find possible unique steps
-      uniqueLists.append(recurseSteps(n, x, []))
+    # call recursive function to find uniqueCount of possible step combinations
+    print(staircase(n, X))
     
     
-    
-  
 if __name__ == "__main__":
     main()

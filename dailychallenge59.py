48b0ee5a6151fa0691ad31cc3037c2065fb5960f
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2018-12-21 17:15:52 -0500

    Create dailychallenge59.py

---
+++ dailychallenge59.py
@@ -0,0 +1,5 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #59
+# Implement a file syncing algorithm for two computers over a low-bandwidth network. What if we know the files 
+# in the two computers are mostly the same?

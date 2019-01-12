377c31a4faee83a72db3a3fbedbb28dada8c476b
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-01-12 14:35:22 -0500

    Create dailychallenge80.py

---
+++ dailychallenge80.py
@@ -0,0 +1,10 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #80
+# Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+#
+#     a
+#    / \
+#   b   c
+#  /
+# d

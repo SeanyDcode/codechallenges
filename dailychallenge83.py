6f934e5d3f34822148dffc9c90f07b686a2ec1a6
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-01-20 20:16:07 -0500

    Create dailychallenge83.py

---
+++ dailychallenge83.py
@@ -0,0 +1,19 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #83
+# Invert a binary tree.
+#
+# For example, given the following tree:
+#
+#     a
+#    / \
+#   b   c
+#  / \  /
+# d   e f
+# should become:
+#
+#   a
+#  / \
+#  c  b
+#  \  / \
+#   f e  d

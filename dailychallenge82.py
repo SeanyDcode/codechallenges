344a5d52f138ea466594464e9113c1526e13ad7f
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-01-20 20:14:55 -0500

    Create dailychallenge82.py

---
+++ dailychallenge82.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #82
+# Using a read7() method that returns 7 characters from a file, implement readN(n) which reads n characters.
+#
+# For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” and then “”.

c2f16ebc23f54a0298f1fb1ef755f6c4e067943d
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-02-23 17:04:09 -0500

    Create dailychallenge120.py

---
+++ dailychallenge120.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #120
+# Implement the singleton pattern with a twist. First, instead of storing one instance, store two instances. 
+# And in every even call of getInstance(), return the first instance and in every odd call of getInstance(), 
+# return the second instance.

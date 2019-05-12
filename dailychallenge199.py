5543a06f45c8372da25c4abb2fedc1f2cfd7cdec
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-05-12 15:58:51 -0400

    Create dailychallenge199.py

---
+++ dailychallenge199.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #199
+# Given a string of parentheses, find the balanced string that can be produced from it using the minimum 
+# number of insertions and deletions. If there are multiple solutions, return any of them.
+#
+# For example, given "(()", you could return "(())". Given "))()(", you could return "()()()()".

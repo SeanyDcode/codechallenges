3964bc81d5cd993543f9c0b324b00c274b3e71a3
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-05-19 14:05:01 -0400

    Create dailychallenge205.py

---
+++ dailychallenge205.py
@@ -0,0 +1,5 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #205
+# Given an integer, find the next permutation of it in absolute order. For example, given 48975, the next 
+# permutation would be 49578.

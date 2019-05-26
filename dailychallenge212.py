8963178ff97157ab3f0720f7a2584b0e9786065a
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-05-26 08:05:11 -0400

    Create dailychallenge212.py

---
+++ dailychallenge212.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #212
+# Spreadsheets often use this alphabetical encoding for its columns: "A", "B", "C", ..., "AA", "AB", ..., "ZZ", 
+# "AAA", "AAB", ....
+#
+# Given a column number, return its alphabetical column id. For example, given 1, return "A". Given 27, return "AA".

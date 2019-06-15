a3288a1694350ca398f3bf37b81a70b810d533c7
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-06-15 14:14:37 -0400

    Create dailychallenge231.py

---
+++ dailychallenge231.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #231
+# Given a string with repeated characters, rearrange the string so that no two adjacent characters are 
+# the same. If this is not possible, return None.
+#
+# For example, given "aaabbc", you could return "ababac". Given "aaab", return None.

a6c295bb951884c49e3e7479cb877788635f5126
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-07-28 11:50:03 -0400

    Create dailychallenge273.py

---
+++ dailychallenge273.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #273
+# A fixed point in an array is an element whose value is equal to its index. Given a sorted array of distinct 
+# elements, return a fixed point, if one exists. Otherwise, return False.
+#
+# For example, given [-6, 0, 2, 40], you should return 2. Given [1, 5, 7, 8], you should return False.

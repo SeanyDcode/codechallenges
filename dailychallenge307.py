7b4aae3863af7e8963d86c64d2c1d278317434bc
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-08-26 19:20:15 -0400

    Create dailychallenge307.py

---
+++ dailychallenge307.py
@@ -0,0 +1,8 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #307
+# Given a binary search tree, find the floor and ceiling of a given integer. The floor is the highest element 
+# in the tree less than or equal to an integer, while the ceiling is the lowest element in the tree greater than 
+# or equal to an integer.
+#
+# If either value does not exist, return None.

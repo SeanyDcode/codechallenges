4fb17947f7ab6fe9ee7a081439fd1b7dd455ae26
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-12-22 12:33:43 -0500

    Create dailychallenge382.py

---
+++ dailychallenge382.py
@@ -0,0 +1,13 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #382
+# Yesterday you implemented a function that encodes a hexadecimal string into Base64.
+#
+# Write a function to decode a Base64 string back to a hexadecimal string.
+#
+# For example, the following string:
+# 
+# 3q2+7w==
+# should produce:
+#
+# deadbeef

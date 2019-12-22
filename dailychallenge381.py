3f5637eac625fc383c0535fee6a646b85c253ade
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2019-12-22 12:32:36 -0500

    Create dailychallenge381.py

---
+++ dailychallenge381.py
@@ -0,0 +1,13 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #381
+# Read this Wikipedia article on Base64 encoding.
+#
+# Implement a function that converts a hex string to base64.
+#
+# For example, the string:
+#
+# deadbeef
+# should produce:
+#
+# 3q2+7w==

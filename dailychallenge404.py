ea268d6917cbf89c250f004dfd594c99cde2feba
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-01-12 15:56:36 -0500

    Create dailychallenge404.py

---
+++ dailychallenge404.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #404
+# Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum 
+# number of rooms required.
+#
+# For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.

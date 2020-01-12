b3bef02869bc3daefdf6f9ea3dbd4520f8b82a80
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-01-12 15:55:08 -0500

    Create dailychallenge402.py

---
+++ dailychallenge402.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #402
+# A strobogrammatic number is a positive number that appears the same after being rotated 180 degrees. For example, 
+# 16891 is strobogrammatic.
+#
+# Create a program that finds all strobogrammatic numbers with N digits.

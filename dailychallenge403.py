b59d5a72f8b5d7cda11f93c6e8cc9ec374da7bed
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-01-12 15:55:52 -0500

    Create dailychallenge403.py

---
+++ dailychallenge403.py
@@ -0,0 +1,5 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #403
+# Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability, implement a 
+# function rand7() that returns an integer from 1 to 7 (inclusive).

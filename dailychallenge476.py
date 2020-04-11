d9782607f1be533f369eda82f2b97b98ad30b1e1
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-04-11 12:39:00 -0400

    Create dailychallenge476.py

---
+++ dailychallenge476.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #476
+# You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}. By the pigeonhole 
+# principle, there must be a duplicate. Find it in linear time and space.
+

b9c66c9e155fd1858a8198c093318172a0261a89
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-04-11 13:03:46 -0400

    Create dailychallenge503.py

---
+++ dailychallenge503.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #503
+# Given a linked list, sort it in O(n log n) time and constant space.
+#
+# For example, the linked list 4 -> 1 -> -3 -> 99 should become -3 -> 1 -> 4 -> 99.

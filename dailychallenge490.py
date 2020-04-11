cbd3dfd9c57257988512368165aa27ba410dd1fc
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-04-11 12:51:49 -0400

    Create dailychallenge490.py

---
+++ dailychallenge490.py
@@ -0,0 +1,12 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #490
+# The horizontal distance of a binary tree node describes how far left or right the node will be when the 
+# tree is printed out.
+#
+# More rigorously, we can define it as follows:
+#
+# The horizontal distance of the root is 0.
+# The horizontal distance of a left child is hd(parent) - 1.
+# The horizontal distance of a right child is hd(parent) + 1.
+# For example, for the following tree, hd(1) = -2, and hd(6) = 0.

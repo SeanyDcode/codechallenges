7ab2634b5cd663cc7425d0e1f4bb75a829ace303
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-04-11 11:51:13 -0400

    Create dailychallenge473.py

---
+++ dailychallenge473.py
@@ -0,0 +1,5 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #473
+# Write an algorithm that computes the reversal of a directed graph. For example, if a graph consists 
+# of A -> B -> C, it should become A <- B <- C.

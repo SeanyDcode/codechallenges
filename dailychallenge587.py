8c4722077e4663c541d19cd92cb7309846263058
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-07-05 17:10:09 -0400

    Create dailychallenge587.py

---
+++ dailychallenge587.py
@@ -0,0 +1,13 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #587
+# Given a binary tree, return all paths from the root to leaves.
+#
+# For example, given the tree:
+#
+#    1
+#   / \
+#  2   3
+#     / \
+#    4   5
+# Return [[1, 2], [1, 3, 4], [1, 3, 5]].

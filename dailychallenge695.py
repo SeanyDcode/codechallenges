3396e285b21e364428996cd220d77097f85e55d5
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2020-11-11 13:32:00 -0500

    Create dailychallenge695.py

---
+++ dailychallenge695.py
@@ -0,0 +1,4 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #695
+# Given an integer n and a list of integers l, write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).

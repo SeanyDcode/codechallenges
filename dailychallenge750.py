83f91ec812bba35c0bf4d73f6d7a6840434d3ae5
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-01-09 17:56:09 -0500

    Create dailychallenge750.py

---
+++ dailychallenge750.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #750
+# Generate a finite, but an arbitrarily large binary tree quickly in O(1).
+#
+# That is, generate() should return a tree whose size is unbounded but finite.

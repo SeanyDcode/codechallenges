d713ef41e97c562b0b45985fe71cc7aeb0dca9e4
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-02-15 18:44:36 -0500

    Create dailychallenge786.py

---
+++ dailychallenge786.py
@@ -0,0 +1,8 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #786
+# Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns x^y.
+#
+# Do this faster than the naive method of repeated multiplication.
+#
+# For example, pow(2, 10) should return 1024.

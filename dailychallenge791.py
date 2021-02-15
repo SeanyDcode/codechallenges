cfb95e790444607b1f71b5c44ad02db793e8cd22
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-02-15 18:50:32 -0500

    Create dailychallenge791.py

---
+++ dailychallenge791.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #791
+# Write a function, throw_dice(N, faces, total), that determines how many ways it is possible to throw N dice with some number 
+# of faces each to get a specific total.
+#
+# For example, throw_dice(3, 6, 7) should equal 15.

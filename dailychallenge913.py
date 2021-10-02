643f2461f5b97b5014282e33f5b4e91cfbe86986
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-10-02 14:34:02 -0400

    Create dailychallenge913.py

---
+++ dailychallenge913.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #913
+# Given a complete binary tree, count the number of nodes in faster than O(n) time. Recall that a complete 
+# binary tree has every level filled except the last, and the nodes in the last level are filled starting 
+# from the left.

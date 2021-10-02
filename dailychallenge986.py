93bd497c66e5ba4ec6b05f63b38724c9ab92765d
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-10-02 18:39:22 -0400

    Create dailychallenge986.py

---
+++ dailychallenge986.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #986
+# Given an integer n, return the length of the longest consecutive run of 1s in its binary representation.
+#
+# For example, given 156, you should return 3.
+

fcd8ea3504157242df1b5850584c28c251fc0925
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-10-02 18:22:26 -0400

    Create dailychallenge961.py

---
+++ dailychallenge961.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #961
+# Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
+#
+# For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".

1fdb2f346efe112e3bb39cbfc1a30b4fcce9fc2e
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-10-02 18:46:03 -0400

    Create dailychallenge998.py

---
+++ dailychallenge998.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #998
+# Given an undirected graph represented as an adjacency matrix and an integer k, write a function to determine 
+# whether each vertex in the graph can be colored such that no two adjacent vertices share the same color using at most k colors.
+

27f057a9579c6a4bcc70414e07259fa45bf9331f
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-12-12 17:19:53 -0500

    Create dailychallenge1045.py

---
+++ dailychallenge1045.py
@@ -0,0 +1,8 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #1045
+# Describe and give an example of each of the following types of polymorphism:
+#
+# Ad-hoc polymorphism
+# Parametric polymorphism
+# Subtype polymorphism

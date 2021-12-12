403813d0804ac3721688e1c9b5916b7dbad8df54
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-12-12 14:52:29 -0500

    Create dailychallenge1023.py

---
+++ dailychallenge1023.py
@@ -0,0 +1,7 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #1023
+# You come across a dictionary of sorted words in a language you've never seen before. Write a program that 
+# returns the correct order of letters in this language.
+#
+# For example, given ['xww', 'wxyz', 'wxyw', 'ywx', 'ywz'], you should return ['x', 'z', 'w', 'y'].

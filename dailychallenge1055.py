56ae9685c487320bc8594e4da699ed9cbeb4120b
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-12-12 18:16:49 -0500

    Create dailychallenge1055.py

---
+++ dailychallenge1055.py
@@ -0,0 +1,13 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #1055
+# Given a node in a binary search tree, return the next bigger element, also known as the inorder successor.
+#
+# For example, the inorder successor of 22 is 30.
+# 
+#    10
+#   /  \
+#  5    30
+#      /  \
+#    22    35
+# You can assume each node has a parent pointer.

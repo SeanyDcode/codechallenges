f03ee451ba3191676465fa838d2522beac7d8294
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2021-12-12 18:20:50 -0500

    Create dailychallenge1060.py

---
+++ dailychallenge1060.py
@@ -0,0 +1,6 @@
+# from dailycodingproblem.com
+#
+# Daily Challenge #1060
+# Given a function f, and N return a debounced f of N milliseconds.
+#
+# That is, as long as the debounced f continues to be invoked, f itself will not be called for N milliseconds.

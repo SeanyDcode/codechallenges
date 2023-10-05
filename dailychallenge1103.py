f0cfb85c2bdf3ef7cc0d73a39448816350017afa
Sean Dougherty <38363783+SeanyDcode@users.noreply.github.com>
2023-10-05 18:38:15 -0400

    Update dailychallenge1103.py

--- dailychallenge1103.py
+++ dailychallenge1103.py
@@ -2,14 +2,22 @@
 #
 # Daily Challenge #1103
 #
-# There are M people sitting in a row of N seats, where M < N. Your task is to redistribute people such that there are 
-# no gaps between any of them, while keeping overall movement to a minimum.
+# You are given an N by M matrix of 0s and 1s. Starting from the top left corner, how many ways are 
+# there to reach the bottom right corner?
 #
-# For example, suppose you are faced with an input of [0, 1, 1, 0, 1, 0, 0, 0, 1], where 0 represents an empty seat and 
-# 1 represents a person. In this case, one solution would be to place the person on the right in the fourth seat. We can 
-# consider the cost of a solution to be the sum of the absolute distance each person must move, so that the cost here would be five.
+# You can only move right and down. 0 represents an empty space while 1 represents a wall you cannot 
+# walk through.
 #
-# Given an input such as the one above, return the lowest possible cost of moving people to remove all gaps.
+# For example, given the following matrix:
+#
+# [[0, 0, 1],
+#  [0, 0, 1],
+#  [1, 0, 0]]
+# Return two, as there are only two ways to get to the bottom right:
+# 
+# Right, down, down, right
+# Down, right, down, right
+# The top left corner and bottom right corner will always be 0.
 #
 # Response from Claude
 # Here is one way to solve this using dynamic programming in Python:
